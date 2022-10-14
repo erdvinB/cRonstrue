6442c09486014a3621984d5622c14907d5551418
erdvinB <101093151+erdvinB@users.noreply.github.com>
2022-10-14 13:59:50 +0700

    Update expressionDescriptor.ts

--- src/expressionDescriptor.ts
+++ src/expressionDescriptor.ts
@@ -623,9 +623,9 @@ export class ExpressionDescriptor {
       if (hour > 12) {
         hour -= 12;
       }
-      if (hour === 0) {
-        hour = 12;
-      }
+//       if (hour === 0) {
+//         hour = 12;
+//       }
     }
 
     const minute = minuteExpression;
